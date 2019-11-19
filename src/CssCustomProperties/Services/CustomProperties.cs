dd3b2edef95091928162bc9a1e3284b85600320e
Peter Blazejewicz <peterblazejewicz@users.noreply.github.com>
2019-11-19 22:03:18 +0100

    :memo: Document access api

--- src/CssCustomProperties/Services/CustomProperties.cs
+++ src/CssCustomProperties/Services/CustomProperties.cs
@@ -4,53 +4,68 @@ using System.Threading.Tasks;
 
 namespace blazejewicz.CssCustomProperties.Services
 {
+    /// <summary>
+    /// Implements access to CSS custom properties
+    /// </summary>
     public class CustomProperties : ICustomProperties
     {
         private const string apiPrefix = "blazejewicz.cssPropertiesSupport";
 
         private readonly IJSRuntime JSRuntime;
 
+        /// <summary>
+        /// Creates instance of the service.
+        /// Note that this is usually created by DI
+        /// </summary>
+        /// <param name="JSRuntime"></param>
         public CustomProperties(IJSRuntime JSRuntime)
         {
             this.JSRuntime = JSRuntime;
         }
 
+        /// <inheritdoc/>
         public async ValueTask<string> GetPropertyValueAsync(ElementReference elementRef, string name)
         {
             const string methodName = "getPropertyValue";
             return await JSRuntime.InvokeAsync<string>($"{apiPrefix}.{methodName}", elementRef, name);
         }
 
+        /// <inheritdoc/>
         public async ValueTask<string> GetPropertyValueAsync(string selector, string name)
         {
             const string methodName = "getPropertyValueWithSelector";
             return await JSRuntime.InvokeAsync<string>($"{apiPrefix}.{methodName}", selector, name);
         }
 
+        /// <inheritdoc/>
         public async ValueTask<string> GetRootPropertyValueAsync(string name)
         {
             const string methodName = "getRootPropertyValue";
             return await JSRuntime.InvokeAsync<string>($"{apiPrefix}.{methodName}", name);
         }
 
+        /// <inheritdoc/>
         public async ValueTask<string> RemovePropertyAsync(ElementReference elementRef, string name)
         {
             const string methodName = "removeProperty";
             return await JSRuntime.InvokeAsync<string>($"{apiPrefix}.{methodName}", elementRef, name);
         }
 
+        /// <inheritdoc/>
         public async ValueTask<string> RemovePropertyAsync(string selector, string name)
         {
             const string methodName = "removePropertyWithSelector";
             return await JSRuntime.InvokeAsync<string>($"{apiPrefix}.{methodName}", selector, name);
         }
 
+        /// <inheritdoc/>
         public async ValueTask<string> RemoveRootPropertyAsync(string name)
         {
             const string methodName = "removeRootProperty";
             return await JSRuntime.InvokeAsync<string>($"{apiPrefix}.{methodName}", name);
         }
 
+        /// <inheritdoc/>
         public ValueTask SetPropertyAsync(ElementReference elementRef, string name, string value, string priority = null)
         {
             const string methodName = "setProperty";
@@ -58,12 +73,14 @@ namespace blazejewicz.CssCustomProperties.Services
             return JSRuntime.InvokeVoidAsync($"{apiPrefix}.{methodName}", elementRef, name, value, priority);
         }
 
+        /// <inheritdoc/>
         public ValueTask SetPropertyAsync(string selector, string name, string value, string priority = null)
         {
             const string methodName = "setPropertyWithSelector";
             return JSRuntime.InvokeVoidAsync($"{apiPrefix}.{methodName}", selector, name, value, priority);
         }
 
+        /// <inheritdoc/>
         public ValueTask SetRootPropertyAsync(string name, string value, string priority = null)
         {
             const string methodName = "setRootProperty";
--- src/CssCustomProperties/Services/ICustomProperties.cs
+++ src/CssCustomProperties/Services/ICustomProperties.cs
@@ -3,23 +3,86 @@ using System.Threading.Tasks;
 
 namespace blazejewicz.CssCustomProperties.Services
 {
+    /// <summary>
+    /// Defins access to CSS custom properties
+    /// See: https://developer.mozilla.org/en-US/docs/Web/CSS/
+    /// </summary>
     public interface ICustomProperties
     {
+        /// <summary>
+        /// Sets custom CSS property scoped to the element
+        /// </summary>
+        /// <param name="elementRef">Reference to the element</param>
+        /// <param name="name">Name of the CSS custom property</param>
+        /// <param name="value">Value of the custom property</param>
+        /// <param name="priority">Optional prority of the custom property</param>
         ValueTask SetPropertyAsync(ElementReference elementRef, string name, string value, string priority = null);
+
+        /// <summary>
+        /// Sets custom CSS property to the element matching selector. The selector is run within the
+        /// context of the document element, which equals to `document.querySelector(selector)` in native
+        /// JavaScript
+        /// </summary>
+        /// <param name="selector">Selector to match element withs</param>
+        /// <param name="name">Name of the CSS custom property</param>
+        /// <param name="value">Value of the custom property</param>
+        /// <param name="priority">Optional priority of the custom property</param>
         ValueTask SetPropertyAsync(string selector, string name, string value, string priority = null);
 
+        /// <summary>
+        /// Sets custom CSS property on the root scope, which equals to `:root` selector.
+        /// </summary>
+        /// <param name="name">Name of the CSS custom property</param>
+        /// <param name="value">Value of the custom property</param>
+        /// <param name="priority">Optional priority of the custom property</param>
         ValueTask SetRootPropertyAsync(string name, string value, string priority = null);
 
+        /// <summary>
+        /// Gets value of the custom CSS property scoped to the element.
+        /// </summary>
+        /// <param name="name">Name of the CSS custom property to retrieve</param>
+        /// <returns>Value of the property as string or empty string</returns>
         ValueTask<string> GetPropertyValueAsync(ElementReference elementRef, string name);
 
+        /// <summary>
+        /// Gets value of the custom CSS propert scoped to the element matched by the
+        /// selector. The selector is run within the context of the document element, which equals to
+        /// `document.querySelector(selector)` in native JavaScript
+        /// </summary>
+        /// <param name="name">Name of the CSS custom property to retrieve</param>
+        /// <returns>Value of the property as string or empty string</returns>
         ValueTask<string> GetPropertyValueAsync(string selector, string name);
 
+        /// <summary>
+        /// Gets value of the custom CSS propert scoped on the root scope, which equals to
+        /// `:root` selector.
+        /// </summary>
+        /// <param name="name">Name of the CSS custom property to retrieve</param>
+        /// <returns>Value of the property as string or empty string</returns>
         ValueTask<string> GetRootPropertyValueAsync(string name);
 
+        /// <summary>
+        /// Removes custom property from the element.
+        /// </summary>
+        /// <param name="elementRef">Reference to the element</param>
+        /// <param name="name">Name of the CSS custom property</param>
+        /// <returns>Value of removed property as string or empty string</returns>
         ValueTask<string> RemovePropertyAsync(ElementReference elementRef, string name);
 
+        /// <summary>
+        /// Removes custom property from the element matched by selector. The selector is run
+        /// in the context of the document, which equals to `document.querySelector(selector)`.
+        /// </summary>
+        /// <param name="selector">Selector to match element withs</param>
+        /// <param name="name">Name of the CSS custom property</param>
+        /// <returns>Value of removed property as string or empty string</returns>
         ValueTask<string> RemovePropertyAsync(string selector, string name);
 
+        /// <summary>
+        /// Removes custom property from the root element.
+        /// </summary>
+        /// <param name="name">Name of the CSS custom property</param>
+        /// <returns>Value of removed property as string or empty string</returns>
         ValueTask<string> RemoveRootPropertyAsync(string name);
 
     }
